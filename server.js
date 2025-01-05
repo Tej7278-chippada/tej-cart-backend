fdb36338649d39e294c5860f7303a186f325d086
Tej7278-chippada <tejachippada45@gmail.com>
2025-01-05 17:02:37 +0530

    local host ip changed

--- server.js
+++ server.js
@@ -164,7 +164,7 @@ app.get('/:id', async (req, res) => {
     }
   });
 const PORT = process.env.PORT || 5009;
-app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port http://192.168.0.172:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port http://192.168.66.172:${PORT}`));
 
 // Connect to MongoDB
 // mongoose.connect(process.env.MONGO_URI)
